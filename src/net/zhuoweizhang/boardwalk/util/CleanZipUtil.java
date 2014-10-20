647a9e7df3d5b8575d01e3065366398e0762fe21
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-10-20 14:51:10 -0400

    Added a function to split a Zip based on number of class files

--- src/net/zhuoweizhang/boardwalk/util/CleanZipUtil.java
+++ src/net/zhuoweizhang/boardwalk/util/CleanZipUtil.java
@@ -1,12 +1,31 @@
 package net.zhuoweizhang.boardwalk.util;
 
+/**
+ * This code is based on JarJar: copyright shown below.
+ * Copyright 2007 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class CleanZipUtil {
@@ -80,5 +99,42 @@ public class CleanZipUtil {
             outputStream.close();
         }
 
+    }
+    public static List<File> shardZip(File from, File tmpDir, int numClasses) throws IOException {
+        byte[] buf = new byte[0x2000];
+        List<File> files = new ArrayList<File>();
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(from));
+        File tmpTo = null;
+        ZipOutputStream out = null;
+        int currentCount = 0;
+        try {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().startsWith("META-INF") || entry.isDirectory()) continue;
+                if (tmpTo == null || currentCount == numClasses) {
+                    if (out != null) out.close();
+                    tmpTo = File.createTempFile(from.getName(), ".jar", tmpDir);
+                    files.add(tmpTo);
+                    out = new ZipOutputStream(new FileOutputStream(tmpTo));
+                    currentCount = 0;
+                }
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                entry.setCompressedSize(-1);
+                out.putNextEntry(entry);
+                IoUtil.pipe(zis, out, buf);
+                if (entry.getName().endsWith(".class")) currentCount++;
+            }
+
+        }
+        finally {
+            //in.close();
+            zis.close();
+            out.close();
+        }
+        return files;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(shardZip(new File(args[0]), new File(args[1]), Integer.parseInt(args[2])));
     }
 }
