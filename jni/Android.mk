7e032b84f999597b301526fc3538908d3a6c14cb
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-04-15 15:10:17 -0400

    Disable the catcher for open source builds

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)  
 LOCAL_LDLIBS := -llog
 LOCAL_MODULE    := boardwalk
-LOCAL_SRC_FILES := main.c catcher.c
+LOCAL_SRC_FILES := main.c
 
 include $(BUILD_SHARED_LIBRARY)
 
